c8fd8c4202a48db7740e67ea962dd2b909b0cbeb
Fabrizio <fabriziocaricato1@gmail.com>
2026-02-01 04:43:09 -0300

    añadido taskRouter y auth.controller

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import express from "express"
 import cors from "cors"
 import { config } from "dotenv"
 import { getUsers, createUser, deleteUser, updateUser } from "./controllers/user.controller.js"
+import { authRouter } from "./router/authRouter.js"
+import { taskRouter } from "./router/taskRouter.js"
 config()
 
 const PORT = process.env.PORT
@@ -13,8 +15,8 @@ servidor.use(express.json())
 servidor.use(cors())
 
 // ENDPOINTS
-servidor.use("/api/auth", )
-servidor.use("/api/tasks", )
+servidor.use("/api/auth", authRouter)
+servidor.use("/api/tasks", taskRouter)
 
 // CONEXIÓN Y ESCUCHA DEL PUERTO
 servidor.listen(PORT, () => {
---
+++ src/router/taskRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const taskRouter = Router()
+
+taskRouter.get("/", )
+taskRouter.post("/", )
+taskRouter.patch("/:id", )
+taskRouter.delete("/:id", )
+
+export {taskRouter}
